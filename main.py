5a0c98484c45ed8544578c35175f0a9a0df8ece7
ARTHUR BUSCARINO BENEDETTI <45307470851@info.local>
2026-03-06 20:11:18 -0300

    Fiz a mediana!!

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 dados_sujos = [10, " erro ", 20, 30, 40, None, 50, 15, " falha ", 25]
 
 def limpar_dados(dados_sujos):
-    # Retorne uma lista apenas com int ou float
     dados_limpos = []
     for dado in dados_sujos:
         if isinstance(dado, (int, float)):
             dados_limpos.append(dado)
     return dados_limpos
 
-def calcular_media():
+def calcular_media(dados):
     pass
 
-def calcular_mediana():
+def calcular_mediana(dados):
+    dados.sort()
+    meio = len(dados) // 2
+    return dados[meio]
+
+def calcular_variancia(dados):
     pass
 
-def calcular_variancia():
-    pass
-
-def obter_extremos():
+def obter_extremos(dados):
     pass
 
 
 dados = limpar_dados(dados_sujos)
-print(f"Dados processados: {dados}")
+
+print("Dados processados:", dados)
+print("Verificado por: Arthur")
