db0c1a21aaf188e46e855ab357db10340fe8779a
VINICIUS DE SOUZA ARAUJO <47627451892@info.local>
2026-03-06 21:19:04 -0300

    Commit feito, com a função extremo feita

--- main.py
+++ main.py
@@ -17,9 +17,26 @@ def calcular_mediana():
 def calcular_variancia():
     pass
 
-def obter_extremos():
-    pass
+def obter_extremos(lista_filtrada):
+    menor_numero = lista_filtrada[0]
+    maior_numero = lista_filtrada[0]
+    
+    for numero in lista_filtrada:
+        if numero < menor_numero:
+            menor_numero = numero
+        
+        elif numero > maior_numero:
+            maior_numero = numero
+    
+    return menor_numero, maior_numero
+    '''menor_numero = min(lista_filtrada)
+    maior_numero = max(lista_filtrada)
+
+    return menor_numero, maior_numero'''
 
 
 dados = limpar_dados(dados_sujos)
+calculo_extr = obter_extremos(dados)
+print(f"Os numeros extremos são: {calculo_extr}")
 print(f"Dados processados: {dados}")
+print("Verificado por: Vinicius")
